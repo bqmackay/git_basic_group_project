7450bd8bdc8f496d76617e5cace42ef6d6c5030f
Byron Mackay <bqmackay@gmail.com>
2021-10-26 16:05:36 -0600

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("This is a simple addition problem.");
+        System.out.println("This is a simple math problem.");
 
         int result = 2 - 1;
 
-        System.out.println("2 - 1 = " + result);
+        System.out.println("2 + 1 = " + result);
     }
 
 }
